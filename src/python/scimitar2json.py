81a4ee59e079ff79c8849344bc522b802bdb0e14
Duncan McColl <duncmc831@gmail.com>
2018-08-22 11:26:40 -0700

    style: comment formatting

--- src/python/scimitar2json.py
+++ src/python/scimitar2json.py
@@ -13,22 +13,19 @@ module_dir='path/to/src/python'
 #import sys
 #sys.path.append(module_dir)
 
-import pickle
-import scimitar2json
-import os
+>> import pickle
+>> import scimitar2json
+>> import os
 
-data_path = os.path.join(
-    module_dir,
-    '../../examples/data/RG_PC_pickle_test.pi'
-)
+>> data_path = os.path.join(module_dir,'../../examples/data/RG_PC_pickle_test.pi')
 
 # Grab the data in the example dir.
-scimitar_model, diff_map = pickle.load(open(data_path, 'rb'))
+>> scimitar_model, diff_map = pickle.load(open(data_path, 'rb'))
 
 # Make a python dictionary of the common format.
-common_dict = scimitar2json.make_dict(scimitar_model, diff_map)
+>> common_dict = scimitar2json.make_dict(scimitar_model, diff_map)
 # Save the dictionary to json format.
-scimitar2json.save_to_json(common_dict,data_path)
+>> scimitar2json.save_to_json(common_dict,data_path)
 """
 import numpy as np
 import json
